f5775165123f91630586664f677e5aa41146e3f6
temamtahir <temam123t@gmail.com>
2023-04-12 04:44:46 -0700

    update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 101-stats.py
+#101-stats.py
 """Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
